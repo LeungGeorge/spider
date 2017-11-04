0f4d1b31b91d794f69e2ddb90e63ea8079bba32b
liangyuanzheng <liangyuanzheng@baidu.com>
2017-11-04 17:49:45 +0800

    add

---
+++ stackoverflow/stackoverflow/items.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class StackoverflowItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    votes = scrapy.Field()
+    answers = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
+
---
+++ stackoverflow/stackoverflow/spiders/question_spider.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from stackoverflow.items import StackoverflowItem
+
+class QuestionSpiderSpider(scrapy.Spider):
+    name = 'question_spider'
+    allowed_domains = ['stackoverflow.com']
+    start_urls = ['http://stackoverflow.com/questions?page=1&sort=votes']
+
+    def parse(self, response):
+        #filename = response.url.split('/')[-2].split('?')[-2] + ".html"
+        #with open(filename, 'wb') as fp:
+            #fp.write(response.body);
+
+        ques = response.xpath('//*[@id="questions"]/div')
+        for q in ques:
+            item = StackoverflowItem()
+            item['title'] = q.xpath('div[2]/h3/a/text()').extract()
+            item['votes'] = q.xpath('div[1]/div[2]/div[1]/div[1]/span[1]/strong[1]/text()').extract()
+            item['answers'] = q.xpath('div[1]/div[2]/div[2]/strong[1]/text()').extract()
+            item['link'] = response.urljoin(q.xpath('div[2]/h3/a/@href').extract()[0])
+            item['desc'] = q.xpath('div[2]/div/text()').extract()
+            yield item
+
+        page = response.xpath('//*[@id="mainbar"]/div[4]/a')
+        for p in page:
+            pageTips = p.xpath('span/text()').extract()
+            pageTipsStrip = pageTips[0].strip()
+            print pageTipsStrip
+            print pageTipsStrip
+            print pageTipsStrip
+            print pageTipsStrip
+                                    
+            if 'next' == pageTipsStrip:
+                nextPageUrl = p.xpath('@href').extract()[0];
+                nextPageUrlFull = response.urljoin(nextPageUrl)
+                print nextPageUrlFull
+                print nextPageUrlFull
+                print nextPageUrlFull
+                print nextPageUrlFull
+                print nextPageUrlFull
+                yield scrapy.Request(nextPageUrlFull, callback=self.parse)
+
